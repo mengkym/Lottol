c65d41f74cc6f81c833d200ef53f5dff72531aac
jinzhaomeng <jinzhaomeng@ts-it.cn>
2021-09-03 17:30:03 +0800

    my codes like a piece of shit

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -7,40 +7,49 @@
 
         <div class="mx-8 mb-6 flex flex-col text-white text-2xl">
           <h1 class="font-bold">Choose</h1>
-          <p>{{len}} numbers</p>
+          <p>{{len}} Numbers</p>
         </div>
 
-        <div class="number-input-wrap h-2/5 px-8 flex items-center space-x-4">
-          <div class="number-cell min-w-1/5 w-1/5 h-4/6 p-3 bg-black bg-opacity-10 rounded-full flex flex-col items-center justify-between text-xl font-semibold"
+        <div class="number-input-wrap h-2/5 px-8 mx-auto overflow-hidden flex items-center justify-center space-x-2">
+          <div class="number-cell p-3 bg-black bg-opacity-10 rounded-full flex-none flex flex-col items-center justify-between text-lg font-semibold"
             :class="{'inputed': !!sortedArr[index - 1]}"
-            v-for="index of len" :key="index">
+            v-for="index of len - 1" :key="index">
             <p>{{sortedArr[index - 1]}}</p>
             <van-icon v-if="sortedArr[index - 1] % 2 == 0" name="fire" color="#ff5959"/>
+          </div>
+          <div class="number-cell p-3 bg-black bg-opacity-50 rounded-full flex-none flex flex-col items-center justify-between text-lg font-semibold"
+            :class="{'inputed': blueNumber}">
+            <p>{{blueNumber}}</p>
           </div>
         </div>
     </div>
     <div class="number-choose-wrap h-3/5 bg-white rounded-3xl rounded-b-none enter-y">
-      <div class="w-full h-full p-8 rounded-3xl shadow">
+      <div class="w-full h-full p-8 rounded-3xl shadow overflow-hidden flex flex-col">
           <div class="flex justify-between">
             <span class="text-xl font-semibold">{{time}}</span>
-            <div class="text-xl space-x-4">
-              <van-icon name="replay" color="#dad7f3" @click="random(0)"/>
-              <van-icon name="cluster-o" color="#dad7f3"/>
+            <div class="text-xl space-x-4 pr-2">
+              <van-icon class="number-choose-tool mr-1" name="replay" color="#dad7f3" @click="random(0)"/>
+              <van-icon class="number-choose-tool" name="cluster-o" color="#dad7f3"/>
             </div>
           </div>
-          <div class="mt-4 grid grid-cols-6 gap-4">
+          <div class="number-block-wrap mt-4 grid grid-cols-6 gap-4">
             <button class="number-block rounded-xl font-semibold"
-              v-for="index of 30" :key="index"
-              :class="{'checked': numbers.indexOf(index)!= -1}"
-              @click="onSelect(index)">
+              v-for="index of endRed" :key="index"
+              :class="{'checkedRed': redNumbers.indexOf(index)!= -1, 'checkedBlue': blueNumber == index}"
+              @click="onSelectRed(index)">
               {{index}}
             </button>
+            <!-- <button class="number-block rounded-xl font-semibold"
+              v-show="isLastOne"
+              v-for="index of endBlue" :key="index"
+              :class="{'checked': blueNumber == index}"
+              @click="onSelectBlue(index)">
+              {{index}}
+            </button> -->
           </div>
-          <div class="1/6">
-            <button class="payment-btn mt-8 rounded w-full h-16 text-white text-lg font-semibold shadow-xl">
+          <button class="payment-btn mt-4 rounded w-full h-16 text-white text-lg font-semibold shadow-xl">
               Make a Payment
-            </button>
-          </div>
+          </button>
       </div>
     </div>
   </div>
@@ -57,15 +66,21 @@ export default defineComponent({
       [Dialog.Component.name]: Dialog.Component
     },
     setup() {
-      const len = ref(5);
-      const end = 30;
-      const numbers: any = ref([]);
+      const len = ref(7);
+      const endRed = 33;
+      const endBlue = 16;
+      const redNumbers: any = ref([]);
+      const blueNumber: any = ref();
       const time: any = ref(moment().format('HH:mm'));
 
       const sortedArr = computed(() => {
-        return numbers.value.sort((a,b)=>{
+        return redNumbers.value.sort((a,b)=>{
           return a - b
         });
+      });
+
+      const isLastOne = computed(() => {
+        return redNumbers.value.length == len.value - 1;
       });
 
       const timer = setInterval(() => { 
@@ -74,29 +89,40 @@ export default defineComponent({
 
       function random(index) {
         let ary: Array<number> = [];
-        for(var i = 0; i < end; i++){
+        for(var i = 0; i < endRed; i++){
           ary[i] = i+1;
         }
         ary.sort(function(){
         　　return 0.5 - Math.random();
         });
-        for(var i = 0; i < len.value ; i++) {
-          numbers.value[i] = ary[i];
+        for(var i = 0; i < len.value - 1 ; i++) {
+          redNumbers.value[i] = ary[i];
         }   
+
+        blueNumber.value = Math.floor(Math.random() * (endBlue - 1)) + 1;
       }
 
-      function onSelect(index: number) {
-        let findIdx = numbers.value.indexOf(index);
+      function onSelectRed(index: number) {
+        let findIdx = redNumbers.value.indexOf(index);
         if(findIdx == -1) {
-          if(numbers.value.length < len.value) {
-            numbers.value.push(index)
+          if(redNumbers.value.length < len.value - 1) {
+            redNumbers.value.push(index)
           } else {
-            numbers.value.pop();
-            numbers.value.push(index);
+            redNumbers.value.pop();
+            redNumbers.value.push(index);
           }
         } else {
-          numbers.value.splice(findIdx, 1);
+          redNumbers.value.splice(findIdx, 1);
         }
+      }
+
+      function onSelectBlue(index: number) {
+        if(blueNumber.value != index) {
+          blueNumber.value = index;
+        } else {
+          blueNumber.value = null;
+        }
+        
       }
 
       onBeforeUnmount(()=>{
@@ -105,12 +131,17 @@ export default defineComponent({
 
       return {
         len,
-        numbers,
+        endRed,
+        endBlue,
+        redNumbers,
+        blueNumber,
         time,
         timer,
         sortedArr,
+        isLastOne,
         random,
-        onSelect
+        onSelectRed,
+        onSelectBlue
       }
     }
 })
@@ -125,6 +156,8 @@ export default defineComponent({
       .number-input-wrap {
         color: @theme-blue;
         .number-cell {
+          width: 2.5rem;
+          height: 5rem;
           &.inputed {
             background: white;
             opacity: 1;
@@ -134,16 +167,36 @@ export default defineComponent({
 
     .number-choose-wrap {
       color: @theme-blue;
-      .number-block {
-        width: 2.5rem;
-        height: 2.5rem;
-        background: #f3f3fb;
-        &.checked {
-          color: white;
-          background: @theme-red;
+      .number-choose-tool {
+        font-size: 26px;
+        &:active {
+          color: @theme-blue !important;
         }
       }
+
+      .number-block-wrap {
+        margin: auto;
+        margin-top: 1rem;
+        .number-block {
+          width: 2.5rem;
+          height: 2.5rem;
+          background: #f3f3fb;
+          &.checkedRed {
+            color: white;
+            background: @theme-red;
+          }
+          &.checkedBlue {
+            color: white;
+            background: @theme-blue;
+          }
+          &.checkedRed.checkedBlue {
+            background: linear-gradient(45deg, @theme-red 0%, @theme-red 50%, @theme-blue 0%, @theme-blue 50%);
+          }
+        }
+      }
+      
       .payment-btn {
+        flex-shrink: 0;
         background: @theme-blue;
       }
     }
